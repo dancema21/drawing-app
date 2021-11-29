823181535e2f97ba56abc5830607f45e20ed9fa5
Dan Cemachovic <dan@libeo.io>
2021-11-29 15:41:43 +0100

    clear rect on all canvas

--- src/components/CanvasActionsBar.js
+++ src/components/CanvasActionsBar.js
@@ -3,7 +3,9 @@ import React from "react";
 const CanvasActionsBar = React.forwardRef((props, ref) => {
   const handleClick = function(){
     const ctx = ref.current.getContext("2d");
-    ctx.clearRect(0, 0, 100, 100);
+    const rect = ref.current.getBoundingClientRect();
+
+    ctx.clearRect(0, 0, rect.width, rect.height);
   }
   
   return <div onClick={handleClick}>Clear</div>;
