b9439465768b7db951e4595942b83afa1096474b
Dan Cemachovic <dan@libeo.io>
2021-12-02 07:55:21 +0100

    download link added

--- src/components/CanvasActionsBar.js
+++ src/components/CanvasActionsBar.js
@@ -1,14 +1,26 @@
 import React from "react";
 
 const CanvasActionsBar = React.forwardRef((props, ref) => {
-  const handleClick = function(){
+  const handleClick = function () {
     const ctx = ref.current.getContext("2d");
     const rect = ref.current.getBoundingClientRect();
 
     ctx.clearRect(0, 0, rect.width, rect.height);
+  };
+
+  let downloadLink;
+  if (ref.current) {
+    downloadLink = ref.current.toDataURL("image/png");
   }
-  
-  return <div onClick={handleClick}>Clear</div>;
+
+  return (
+    <div>
+      <div onClick={handleClick}>Clear</div>
+      <a href={downloadLink} download="my-drawing">
+        Download
+      </a>
+    </div>
+  );
 });
 
 export default CanvasActionsBar;
